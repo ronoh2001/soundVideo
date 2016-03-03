6d16a6ea5e34759145f9c04e987aceaa6980155b
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-03-02 21:17:51 -0800

    Fix server to serve static files; rename mainPage.html to index.html

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require ('express');
 var app = express();
-var path = require('path');
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname +'/mainPage.html'));
-});
+
+app.use(express.static(__dirname));
+
 app.listen(8080);
