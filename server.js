1c13586785b3dfbe6af5cda8a160715908443320
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-03-02 21:02:20 -0800

    create server

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require ('express');
+var app = express();
+var path = require('path');
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname +'/mainPage.html'));
+});
+app.listen(8080);
